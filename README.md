6c656f038c3e82ef70d0017c33595c5bb002f034
Marcus Edwards <marcusstephenedwards@gmail.com>
2025-05-18 20:52:24 -0400

    Add ordered by ref appearance option

--- README.md
+++ README.md
@@ -10,3 +10,11 @@ python3 create_chapter_bibliographies.py -c chapter1.tex -m bibliography.bib
 ```
 
 The tool will then scan the chapter TeX file for in-line references and pull them out of the master .bib file into a chapter .bib file named in the same way as the chapter TeX file.
+The entries in the chapter bibliography will appear in the same order as they did in the master bibliography.
+
+Alternatively, the following script can be called, which will order the entries in the chapter bibliography by the order that
+the references to them appear in the chapter source.
+
+```
+python3 create_ordered_chapter_bibliographies.py -c chapter1.tex -m bibliography.bib
+```
---
+++ create_ordered_chapter_bibliographies.py
@@ -0,0 +1,90 @@
+import sys
+import getopt
+import re
+
+HELP_STRING = """
+Creates a Bibtex chapter bibliography file by scanning the chapter source for references to the 
+sources listed in the master Bibtex bibliography. The chapter bibliography contains entries ordered
+by their appearance in the chapter source.
+"""
+
+
+def main(argv):
+    chapter_file = None
+
+    try:
+        opts, args = getopt.getopt(
+            argv,
+            "c:m:",
+            ["chapter_source=", "master_bibliography="]
+        )
+    except getopt.GetoptError:
+        print(HELP_STRING)
+        sys.exit(2)
+
+    # comment out this block to test
+    for opt, arg in opts:
+        if opt in ["-h", "--help"]:
+            print(HELP_STRING)
+            sys.exit()
+        elif opt in ["-c", "--chapter_source"]:
+            chapter_file = arg
+        elif opt in ["-m", "--master_bibliography"]:
+            master_file = arg
+
+    if chapter_file is not None and master_file is not None:
+        chapter_bib = []
+
+        print('\x1b[34m Scanning the chapter TeX file for references... \n \x1b[37m')
+        chp_handle = open(chapter_file, 'r')
+        chp = chp_handle.read()
+        ref_pos = [match.start() for match in re.finditer(r'\\cite', chp)]
+        print(f'Found {len(ref_pos)} in-line references \n')
+
+        print('\x1b[34m Comparing the references to the master bibliography... \n \x1b[37m')
+        mstr_handle = open(master_file, 'r')
+        mstr = mstr_handle.read()
+        lines = mstr.split("\n")
+        for chp_pos in ref_pos:
+            pos_refs = chp[chp_pos + 6: chp_pos + chp[chp_pos:].index('}')].split(',')
+            for pos_ref in pos_refs:
+                pos = 0
+                while pos < len(lines):
+                    line = lines[pos]
+                    if '@' in line:
+                        ref_info = f"\n{line}"
+                        ref_str = line.split('{')[1].split(',')[0]  # strip @inproceedings or @article, etc. and ,
+                        found = False
+                        if pos_ref.strip() == ref_str:  # if the ref is in the chapter
+                            found = True
+                            print(f'Found ref {ref_str} \n')
+                            while line and line[0] != '}':
+                                pos += 1
+                                line = lines[pos]
+                                ref_info += f"\n{line}"
+                            chapter_bib.append(ref_info)
+                            break
+                    if found:
+                        break
+                    pos += 1
+                if not found:
+                    print(f'No match for {ref_str} in {chapter_file} \n')
+
+
+        print('\x1b[34m Writing the matching references to the chapter bibliography... \n \x1b[37m')
+        chp_bib_handle = open(f"{chapter_file.split('.')[0]}.bib", 'w')
+        chp_bib_handle.write('\n'.join(chapter_bib))
+
+        print('\x1b[34m Done! Cleaning up... \n \x1b[37m')
+        chp_handle.close()
+        mstr_handle.close()
+        chp_bib_handle.close()
+
+        print('\x1b[34m ~*Squeaky clean*~ \n \x1b[37m')
+
+    else:
+        print("The chapter file and master bibliography file are required parameters.")
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
